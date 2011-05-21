c034d835a0e916da940f5fa63ccbef6a0b2b4717
Matthaus Litteken <mlitteken@gmail.com>
2011-05-21 02:11:06 -0700

    Improved ruby clarity. Modified to handle multiple patterns in the pattern file.

--- kmp.rb
+++ kmp.rb
@@ -1,33 +1,37 @@
 class KnuthMorrisPratt
   def initialize
     @text = String.new
-    @pattern = String.new
+    @pattern = Array.new
     @sourceText
     @sourcePattern
-    @pi = []
+    @pi = Array.new
     @debug = true
-    @m
+    @m = Array.new
+    @n
   end
   
   def setup(sourceText, sourcePattern)
+    line = 0
+    
     # Check that the files exist
     if (sourceText and sourcePattern and File.file?(sourceText) and File.file?(sourcePattern))
       # First load the text into a string
-      data = File.open(sourceText, "r") do |file|
-        while curLine = file.gets
-          @text << curLine
+      File.open(sourceText, "r").each_line do |curLine|
+        if (curLine and curLine.chomp)
+          @text << curLine.chomp
         end
       end
     
       # Now load the pattern
-      data = File.open(sourcePattern, "r") do |file|
-        while curLine = file.gets
-          @pattern << curLine
+      File.open(sourcePattern, "r").each_line do |curLine|
+        if (curLine and curLine.chomp)
+          @pattern[line] = curLine.chomp
+          @m[line] = @pattern[line].length
+          line = line + 1
         end
       end
   
       @n = @text.length
-      @m = @pattern.length
       prefix
     else
       puts "First argument should be the data file, second argument should be the search pattern."
@@ -35,63 +39,74 @@ class KnuthMorrisPratt
   end
 
   def search
-    textPos = 0
-    patternPos = 0
-    comparisons = 0
-    found = false
+    totalComps = 0
     
-    while (textPos + patternPos) < @n
-      #puts @pattern[patternPos].chr << " vs " << @text[textPos + patternPos].chr
+    for i in (0..@pattern.length - 1)
+      puts "seaching for pattern ##{i+1} (#{@pattern[i]})"
+
+      textPos = 0
+      patternPos = 0
+      comparisons = 0
+      found = false
+    
+      while (textPos + patternPos) < @n
+        #puts @pattern[patternPos].chr << " vs " << @text[textPos + patternPos].chr
       
-      if ((comparisons = comparisons + 1) and @pattern[patternPos] == @text[textPos + patternPos])
+        if ((comparisons = comparisons + 1) and @pattern[i][patternPos] == @text[textPos + patternPos])
         
-        if patternPos == (@m - 1)
-          found = true
-          puts "word found at " << textPos.to_s
-          patternPos = 0
-          textPos = textPos + @m
-        end
+          if patternPos == (@m[i] - 1)
+            found = true
+            puts "word found at " << textPos.to_s
+            patternPos = 0
+            textPos = textPos + @m[i]
+          end
         
-        patternPos = patternPos + 1
+          patternPos = patternPos + 1
       
-      else
-      
-        textPos = textPos + patternPos - @pi[patternPos]
-      
-        if @pi[patternPos] > -1
-          patternPos = @pi[patternPos]
         else
-          patternPos = 0
-        end
       
+          textPos = textPos + patternPos - @pi[i][patternPos]
+      
+          if @pi[i][patternPos] > -1
+            patternPos = @pi[i][patternPos]
+          else
+            patternPos = 0
+          end
+      
+        end
+    
       end
     
+      if (!found)
+        puts "word not found"
+      end
+    
+      puts "number of comparisons: " << comparisons.to_s
+      totalComps = totalComps + comparisons
     end
     
-    if (!found)
-      puts "word not found"
-    end
+    puts "total number of comparisons: " << totalComps.to_s
     
-    puts "number of comparisons: " << comparisons.to_s
-  
   end
 
   def prefix
-    # Initialization of @pi
-    @pi = [-1, 0]
-    pos = 2
-    cnd = 0
+    for i in (0..@pattern.length - 1)
+      # Initialization of @pi
+      @pi[i] = [-1, 0]
+      pos = 2
+      cnd = 0
   
-    while (pos < @m) #and @pattern[k + 1] != @pattern[q])
-      if @pattern[pos - 1] == @pattern[cnd]
-        cnd = cnd + 1
-        @pi[pos] = cnd
-        pos = pos + 1
-      elsif cnd > 0
-        cnd = @pi[cnd]
-      else
-        @pi[pos] = 0
-        pos = pos + 1
+      while (pos < @m[i])
+        if @pattern[i][pos - 1] == @pattern[i][cnd]
+          cnd = cnd + 1
+          @pi[i][pos] = cnd
+          pos = pos + 1
+        elsif cnd > 0
+          cnd = @pi[i][cnd]
+        else
+          @pi[i][pos] = 0
+          pos = pos + 1
+        end
       end
     end
   end
