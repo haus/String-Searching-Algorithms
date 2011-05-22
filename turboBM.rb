e9cb16ad43773fab0a2ba90e7e78f05207d1c9b7
Matthaus Litteken <mlitteken@gmail.com>
2011-05-22 16:15:03 -0700

    So turns out I didn't understand some bits of ruby syntax. Now I do. And the turbo boyer moore works. And works well. Next up, a comparison wrapper.

--- turboBM.rb
+++ turboBM.rb
@@ -4,54 +4,35 @@ class TurboBoyerMoore
   def initialize
     @text = String.new
     @pattern = Array.new
-    @sourceText
-    @sourcePattern
-    @good = Array.new
-    @bad = Array.new
     @suff = Array.new
-    @alpha = Array.new
-    @debug = true
-    @m = Array.new
+    @debug = false
+    @m
     @n
   end
   
   def setup(sourceText, sourcePattern)
-    line = 0
-    
-    # Check that the files exist
-    if (sourceText and sourcePattern and File.file?(sourceText) and File.file?(sourcePattern))
-      # First load the text into a string
-      File.open(sourceText, "r").each_line do |curLine|
-        if (curLine and curLine.chomp)
-          @text << curLine.chomp
-        end
-      end
-    
-      # Now load the pattern
-      File.open(sourcePattern, "r").each_line do |curLine|
-        if (curLine and curLine.chomp)
-          @pattern[line] = curLine.chomp
-          @m[line] = @pattern[line].length
-          line = line + 1
-        end
-      end
+    @text = sourceText
+    @pattern = sourcePattern
   
-      @n = @text.length
-      puts @pattern.to_set.length.chr
-      getSuffix
-      gPrefix
-      bPrefix
-    else
-      puts "First argument should be the data file, second argument should be the search pattern."
+    @n = @text.length
+    @m = @pattern.length
+    
+    suffix
+    gPrefix
+    bPrefix
+    if @debug
+      @suff.each_with_index do |suffix, i|
+        puts "Suffix #{i} is #{suffix}"
+      end
+      @good.each_with_index do |gd, i|
+        puts "Good #{i} is #{gd}"
+      end
+      @bad.each do |key, val|
+        puts "Bad #{key} is #{val}"
+      end
     end
   end
   
-  def search
-    
-    
-  end
-  
-  def getSuffix
 =begin
     void suffixes(char *x, int m, int *suff) {
        int f, g, i;
@@ -72,34 +53,30 @@ class TurboBoyerMoore
        }
     }
 =end
-    for i in (0..@pattern.length - 1)
-      @suff[i] = Array.new
-      
-      @suff[i][@m[i].length-1] = @m[i].length
-      
-      g = @m[i].length - 1
-      
-      for j in ((@m[i].length - 2)..0)
-        if (j > g and @suff[i][j + @m[i].length - 1 - f] < (j - g))
-          @suff[i][j] = @suff[i][j + @m[i].length - 1 - f]
-        else
-          if (j < g)
-            g = j
-          end
-          
-          f = j
-          
-          while (g >= 0 and @pattern[i][g] == @pattern[i][g + @m[i].length - 1 - f])
-            g -= 1
-          end
-          
-          @suff[i][j] = f - g
+  def suffix
+    @suff[@m - 1] = @m
+    g = @m - 1
+    f = 0#2**(32)-1
+    
+    (@m - 2).downto(0) do |i|
+      if (i > g and @suff[i + @m - 1 - f] < (i - g))
+        @suff[i] = @suff[i + @m - 1 - f]
+      else
+        if (i < g)
+          g = i
         end
+        
+        f = i
+        
+        while (g >= 0 and @pattern[g] == @pattern[g + @m - 1 - f])
+          g -= 1
+        end
+        
+        @suff[i] = f - g
       end
     end
   end
   
-  def gPrefix
 =begin
   void preBmGs(char *x, int m, int bmGs[]) {
      int i, j, suff[XSIZE];
@@ -118,32 +95,30 @@ class TurboBoyerMoore
         bmGs[m - 1 - suff[i]] = m - 1 - i;
   }
 =end
-    for i in (0..@pattern.length - 1)
-      @good[i] = Array.new
-      
-      for j in (0..@m[i].length - 1)
-        @good[i][j] = @m[i].length
-      end
-      
-      k = 0
-      
-      for j in (@m[i].length - 1..0)
-        if (@suff[i][j] == (j + 1))
-          k.upto(@m[i].length - 1 - j) {
-            if @good[i][k] == @m[i].length
-              @good[i][k] = @m[i].length - 1 -j
-            end
-          }
+  def gPrefix
+    @good = Array.new
+    
+    0.upto(@m - 1) do |i|
+      @good[i] = @m
+    end
+    
+    k = 0
+    
+    (@m - 1).downto(0) do |i|
+      if (@suff[i] == (i + 1))
+        k.upto(@m - 2 - i) do |j|
+          if @good[j] == @m
+            @good[j] = @m - 1 - i
+          end
         end
       end
-        
-      for j in (0..@m[i].length - 2)
-        @good[i][@m[i].length - 1 - @suff[i][j]] = @m[i].length - 1 - j
-      end
+    end
+      
+    0.upto(@m - 2) do |i|
+      @good[@m - 1 - @suff[i]] = @m - 1 - i
     end
   end
-  
-  def bPrefix
+    
 =begin
     void preBmBc(char *x, int m, int bmBc[]) {
        int i;
@@ -154,67 +129,29 @@ class TurboBoyerMoore
           bmBc[x[i]] = m - i - 1;
     }
 =end
-
-    for i in (0..@pattern.length - 1)
-      @bad[i] = Hash.new
-      @alpha[i] = Set.new
-      
-      for j in (0..@pattern[i].length - 1)
-        @alpha[i].add(@pattern[i][j])
+  def bPrefix
+    @bad = Hash.new
+    @alpha = Set.new
+    
+    0.upto(@n - 1) do |i|
+      @alpha.add(@text[i].chr)
+    end
+    
+    if @debug
+      @alpha.each_with_index do |a, i|
+        puts "Element #{i} is #{a}"
       end
-      
-      for j in (0..@alpha[i].size)
-        @bad[i][j] = @m[i]
-      end
-      
-      for j in (0..(@m[i] - 1))
-        @bad[i][@pattern[j]] = @m[i] - j - 1
-      end
+    end
+    
+    @alpha.each_with_index do |a, i|
+      @bad[a] = @m
+    end
+    
+    0.upto(@m - 1) do |i|
+      @bad[@pattern[i].chr] = @m - i - 1
     end
   end
-  
-  def search
-    for i in (0..@pattern.length - 1)
-      j = 0
-      u = 0
-      
-      shift = @m[i].length
-      
-      while j <= (@n.length - @m[i].length)
-        k = @m[i].length - 1
-        
-        while (k >= 0 and @pattern[i][k] == @text[k + j])
-          k -= 1
-          
-          if (u != 0 and k == (@m[i].length - 1 - shift))
-            k -= u
-          end
-        end
-        
-        if (k < 0)
-          puts "match found at #{j}"
-          shift = @good[i][0]
-          u = @m[i].length - shift
-        else
-          v = @m[i].length -1 - k
-          turboShift = u - v
-          bcShift = @bad[i][@text[k + j]] - @m[i].length + 1 + k
-          shift = [turboShift, bcShift, @good[i][k]].max
-          
-          if (shift == @good[i][k])
-            u = [@m[i].length - shift, v].min
-          else
-            if (turboShift < bcShift)
-              shift = [shift, u + 1].max
-            end
-            u = 0
-          end
-        end
-        
-        j += shift
-        
-      end
-    end
+
 =begin
   void TBM(char *x, int m, char *y, int n) {
     int bcShift, i, j, shift, u, v, turboShift,
@@ -256,9 +193,92 @@ class TurboBoyerMoore
      }
   }
 =end
+  def search
+    comparisons = 0
+    found = false
+    j = 0
+    u = 0
+    
+    shift = @m
+    
+    while j <= (@n - @m)
+      i = @m - 1
+      
+      while ((comparisons += 1) and i >= 0 and @pattern[i] == @text[i + j])
+        i -= 1
+        
+        if (u != 0 and i == (@m - 1 - shift))
+          i -= u
+        end
+      end
+      
+      if (i < 0)
+        found = true
+        puts "match found at #{j}"
+        shift = @good[0]
+        u = @m - shift
+      else
+        v = @m - 1 - i
+        turboShift = u - v
+        bcShift = @bad[@text[i + j].chr] - @m + 1 + i
+        shift = [turboShift, bcShift, @good[i]].max
+        
+        if (shift == @good[i])
+          u = [@m - shift, v].min
+        else
+          if (turboShift < bcShift)
+            shift = [shift, u + 1].max
+          end
+          u = 0
+        end
+      end
+      
+      j += shift
+      
+    end
+    
+    if !found
+      puts "search string not found"
+    end
+    
+    puts "#{comparisons} were used in this search"
+    
+    return comparisons
   end
 end
 
-tbm = TurboBoyerMoore.new
-tbm.setup(ARGV[0], ARGV[1])
-tbm.search
+text = String.new
+pattern = Array.new
+line = 0
+
+# Check that the files exist
+if (ARGV[0] and ARGV[1] and File.file?(ARGV[0]) and File.file?(ARGV[1]))
+  # First load the text into a string
+  File.open(ARGV[0], "r").each_line do |curLine|
+    if (curLine and curLine.chomp)
+      text << curLine.chomp
+    end
+  end
+
+  # Now load the pattern
+  File.open(ARGV[1], "r").each_line do |curLine|
+    if (curLine and curLine.chomp and curLine.chomp.length > 0)
+      pattern[line] = curLine.chomp
+      line += 1
+    end
+  end
+else
+  puts "First argument should be the data file, second argument should be the search pattern."
+end
+
+total = 0
+
+pattern.each_with_index do |patt, i|
+  puts "Search ##{i+1}: searching text for #{patt}"
+  tbm = TurboBoyerMoore.new
+  tbm.setup(text, patt)
+  total += tbm.search
+end
+
+puts "total number of comparisons: " << total.to_s
+
