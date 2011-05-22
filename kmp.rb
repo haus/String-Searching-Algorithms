328dc6c0ef1c8ae80fa399bdabf093a088aedb90
Matthaus Litteken <mlitteken@gmail.com>
2011-05-22 12:47:26 -0700

    Decoupled file handling from main class. Now it takes a string text and pattern to search for. It returns the number of comparisons as well as any matches.

--- kmp.rb
+++ kmp.rb
@@ -1,112 +1,73 @@
 class KnuthMorrisPratt
   def initialize
     @text = String.new
-    @pattern = Array.new
-    @sourceText
-    @sourcePattern
+    @pattern = String.new
     @pi = Array.new
-    @debug = true
-    @m = Array.new
+    @m
     @n
   end
   
   def setup(sourceText, sourcePattern)
     line = 0
-    
-    # Check that the files exist
-    if (sourceText and sourcePattern and File.file?(sourceText) and File.file?(sourcePattern))
-      # First load the text into a string
-      File.open(sourceText, "r").each_line do |curLine|
-        if (curLine and curLine.chomp)
-          @text << curLine.chomp
-        end
-      end
-    
-      # Now load the pattern
-      File.open(sourcePattern, "r").each_line do |curLine|
-        if (curLine and curLine.chomp and curLine.chomp.length > 0)
-          @pattern[line] = curLine.chomp
-          @m[line] = @pattern[line].length
-          line = line + 1
-        end
-      end
-  
-      @n = @text.length
-      prefix
-    else
-      puts "First argument should be the data file, second argument should be the search pattern."
-    end
+    @text = sourceText
+    @pattern = sourcePattern
+    @m = @pattern.length
+    @n = @text.length
+    prefix
   end
 
   def search
-    totalComps = 0
+    textPos = 0
+    patternPos = 0
+    comparisons = 0
+    found = false
+  
+    while (textPos + patternPos) < @n
+      #puts @pattern[patternPos].chr << " vs " << @text[textPos + patternPos].chr
     
-    for i in (0..@pattern.length - 1)
-      puts "seaching for pattern ##{i+1} (#{@pattern[i]})"
-
-      textPos = 0
-      patternPos = 0
-      comparisons = 0
-      found = false
-    
-      while (textPos + patternPos) < @n
-        #puts @pattern[patternPos].chr << " vs " << @text[textPos + patternPos].chr
-      
-        if ((comparisons = comparisons + 1) and @pattern[i][patternPos] == @text[textPos + patternPos])
-        
-          if patternPos == (@m[i] - 1)
-            found = true
-            puts "word found at " << textPos.to_s
-            patternPos = 0
-            textPos = textPos + @m[i]
-          end
-        
-          patternPos = patternPos + 1
-      
-        else
-      
-          textPos = textPos + patternPos - @pi[i][patternPos]
-      
-          if @pi[i][patternPos] > -1
-            patternPos = @pi[i][patternPos]
-          else
-            patternPos = 0
-          end
-      
+      if ((comparisons = comparisons + 1) and @pattern[patternPos] == @text[textPos + patternPos])
+        if patternPos == (@m - 1)
+          found = true
+          puts "word found at " << textPos.to_s
+          patternPos = 0
+          textPos = textPos + @m
         end
-    
+        patternPos = patternPos + 1
+      else
+        textPos = textPos + patternPos - @pi[patternPos]
+        if @pi[patternPos] > -1
+          patternPos = @pi[patternPos]
+        else
+          patternPos = 0
+        end
       end
-    
-      if (!found)
-        puts "word not found"
-      end
-    
-      puts "number of comparisons: " << comparisons.to_s
-      totalComps = totalComps + comparisons
     end
     
-    puts "total number of comparisons: " << totalComps.to_s
+    if (!found)
+      puts "word not found"
+    end
+  
+    puts "number of comparisons: " << comparisons.to_s
     
+    return comparisons
   end
 
   def prefix
-    for i in (0..@pattern.length - 1)
-      # Initialization of @pi
-      @pi[i] = [-1, 0]
-      pos = 2
-      cnd = 0
-  
-      while (pos < @m[i])
-        if @pattern[i][pos - 1] == @pattern[i][cnd]
-          cnd = cnd + 1
-          @pi[i][pos] = cnd
-          pos = pos + 1
-        elsif cnd > 0
-          cnd = @pi[i][cnd]
-        else
-          @pi[i][pos] = 0
-          pos = pos + 1
-        end
+    # Initialization of @pi
+    @pi = [-1, 0]
+    pos = 2
+    cnd = 0
+
+    while (pos < @m)
+      if @pattern[pos - 1] == @pattern[cnd]
+        cnd = cnd + 1
+        @pi[pos] = cnd
+        pos = pos + 1
+      elsif cnd > 0
+        cnd = @pi[cnd]
+      else
+        @pi[pos] = 0
+        pos = pos + 1
       end
     end
   end
@@ -119,7 +80,37 @@ class KnuthMorrisPratt
   
 end
 
-kmp = KnuthMorrisPratt.new
-kmp.setup(ARGV[0], ARGV[1])
-#kmp.printPi
-kmp.search
+text = String.new
+pattern = Array.new
+line = 0
+
+# Check that the files exist
+if (ARGV[0] and ARGV[1] and File.file?(ARGV[0]) and File.file?(ARGV[1]))
+  # First load the text into a string
+  File.open(ARGV[0], "r").each_line do |curLine|
+    if (curLine and curLine.chomp)
+      text << curLine.chomp
+    end
+  end
+
+  # Now load the pattern
+  File.open(ARGV[1], "r").each_line do |curLine|
+    if (curLine and curLine.chomp and curLine.chomp.length > 0)
+      pattern[line] = curLine.chomp
+      line += 1
+    end
+  end
+else
+  puts "First argument should be the data file, second argument should be the search pattern."
+end
+
+total = 0
+
+pattern.each_with_index do |patt, i|
+  kmp = KnuthMorrisPratt.new
+  kmp.setup(text, patt)
+  #kmp.printPi
+  total += kmp.search
+end
+
+puts "total number of comparisons: " << total.to_s
